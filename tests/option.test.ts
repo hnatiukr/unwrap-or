f6bf18882d21eea502b788c3d07b44673805a4ff
Roman Hnatiuk <hnatiukr@pm.me>
2025-06-19 08:17:20 +0200

    tests(option): use suite describe tests

--- tests/option.test.ts
+++ tests/option.test.ts
@@ -1,375 +1,377 @@
-import { test, assert } from "vitest";
+import { describe, test, assert } from "vitest";
 
 import { Some, None, type Option } from "../src/option";
 
 const assert_eq = assert.deepEqual;
 const assert_err = assert.throw;
 
-test("Option :: and", () => {
-  let x: Option<number>;
-  let y: Option<string>;
+describe("Option", () => {
+  test("and", () => {
+    let x: Option<number>;
+    let y: Option<string>;
 
-  x = Some(2);
-  y = None;
-  assert_eq!(x.and(y), None);
+    x = Some(2);
+    y = None;
+    assert_eq!(x.and(y), None);
 
-  x = None;
-  y = Some("foo");
-  assert_eq!(x.and(y), None);
+    x = None;
+    y = Some("foo");
+    assert_eq!(x.and(y), None);
 
-  x = Some(2);
-  y = Some("foo");
-  assert_eq!(x.and(y), Some("foo"));
+    x = Some(2);
+    y = Some("foo");
+    assert_eq!(x.and(y), Some("foo"));
 
-  x = None;
-  y = None;
-  assert_eq!(x.and(y), None);
-});
-
-test("Option :: and_then", () => {
-  let x: Option<string>;
-  let y: Option<string>;
-
-  x = Some("some value");
-  y = None;
-  assert_eq!(
-    x.and_then(() => y),
-    None,
-  );
-
-  x = None;
-  y = Some("then value");
-  assert_eq!(
-    x.and_then(() => y),
-    None,
-  );
-
-  x = Some("some value");
-  y = Some("then value");
-  assert_eq!(
-    x.and_then(() => y),
-    Some("then value"),
-  );
-
-  x = None;
-  y = None;
-  assert_eq!(
-    x.and_then(() => y),
-    None,
-  );
-});
-
-test("Option :: expect", () => {
-  let x: Option<string>;
-
-  x = Some("value");
-  assert_eq!(x.expect("should return string value"), "value");
-
-  x = None;
-  assert_err!(
-    () => x.expect("should return string value"),
-    Error,
-    "should return string value",
-  );
-});
-
-test("Option :: filter", () => {
-  function is_even(n: number): boolean {
-    return n % 2 == 0;
-  }
-
-  let x: Option<number>;
-
-  x = None;
-  assert_eq!(x.filter(is_even), None);
-
-  x = Some(3);
-  assert_eq!(x.filter(is_even), None);
-
-  x = Some(4);
-  assert_eq!(x.filter(is_even), Some(4));
-});
-
-test("Option :: flatten", () => {
-  let x: Option<Option<number>>;
-
-  x = Some(Some(6));
-  assert_eq!(x.flatten(), Some(6));
-
-  x = Some(None);
-  assert_eq!(x.flatten(), None);
-
-  x = None;
-  assert_eq!(x.flatten(), None);
-});
-
-test("Option :: inspect", () => {
-  function get<T>(arr: T[], idx: number): Option<T> {
-    const item = arr.at(idx);
-    return item !== undefined ? Some(item) : None;
-  }
-
-  const list = [1, 2, 3, 4, 5];
-
-  let has_inspected = false;
-
-  let x = get(list, 2).inspect((_v) => {
-    has_inspected = true;
+    x = None;
+    y = None;
+    assert_eq!(x.and(y), None);
   });
 
-  assert_eq!(x, Some(3));
-  assert_eq!(has_inspected, true);
-});
+  test("and_then", () => {
+    let x: Option<string>;
+    let y: Option<string>;
 
-test("Option :: is_none", () => {
-  let x: Option<number>;
+    x = Some("some value");
+    y = None;
+    assert_eq!(
+      x.and_then(() => y),
+      None,
+    );
 
-  x = Some(2);
-  assert_eq!(x.is_none(), false);
+    x = None;
+    y = Some("then value");
+    assert_eq!(
+      x.and_then(() => y),
+      None,
+    );
 
-  x = None;
-  assert_eq!(x.is_none(), true);
-});
+    x = Some("some value");
+    y = Some("then value");
+    assert_eq!(
+      x.and_then(() => y),
+      Some("then value"),
+    );
 
-test("Option :: is_none_or", () => {
-  let x: Option<number>;
+    x = None;
+    y = None;
+    assert_eq!(
+      x.and_then(() => y),
+      None,
+    );
+  });
 
-  x = Some(2);
-  assert_eq!(
-    x.is_none_or((v) => v > 1),
-    true,
-  );
+  test("expect", () => {
+    let x: Option<string>;
 
-  x = Some(0);
-  assert_eq!(
-    x.is_none_or((v) => v > 1),
-    false,
-  );
+    x = Some("value");
+    assert_eq!(x.expect("should return string value"), "value");
 
-  x = None;
-  assert_eq!(
-    x.is_none_or((v) => v > 1),
-    true,
-  );
-});
+    x = None;
+    assert_err!(
+      () => x.expect("should return string value"),
+      Error,
+      "should return string value",
+    );
+  });
 
-test("Option :: is_some", () => {
-  let x: Option<number>;
+  test("filter", () => {
+    function is_even(n: number): boolean {
+      return n % 2 == 0;
+    }
 
-  x = Some(2);
-  assert_eq!(x.is_some(), true);
+    let x: Option<number>;
 
-  x = None;
-  assert_eq!(x.is_some(), false);
-});
+    x = None;
+    assert_eq!(x.filter(is_even), None);
 
-test("Option :: is_some_and", () => {
-  let x: Option<number>;
+    x = Some(3);
+    assert_eq!(x.filter(is_even), None);
 
-  x = Some(2);
-  assert_eq!(
-    x.is_some_and((v) => v > 1),
-    true,
-  );
+    x = Some(4);
+    assert_eq!(x.filter(is_even), Some(4));
+  });
 
-  x = Some(0);
-  assert_eq!(
-    x.is_some_and((v) => v > 1),
-    false,
-  );
+  test("flatten", () => {
+    let x: Option<Option<number>>;
 
-  x = None;
-  assert_eq!(
-    x.is_some_and((v) => v > 1),
-    false,
-  );
-});
+    x = Some(Some(6));
+    assert_eq!(x.flatten(), Some(6));
 
-test("Option :: map", () => {
-  let x: Option<string>;
+    x = Some(None);
+    assert_eq!(x.flatten(), None);
 
-  x = Some("Hello, World!");
-  assert_eq!(
-    x.map((s) => s.length),
-    Some(13),
-  );
+    x = None;
+    assert_eq!(x.flatten(), None);
+  });
 
-  x = None;
-  assert_eq!(
-    x.map((s) => s.length),
-    None,
-  );
-});
+  test("inspect", () => {
+    function get<T>(arr: T[], idx: number): Option<T> {
+      const item = arr.at(idx);
+      return item !== undefined ? Some(item) : None;
+    }
 
-test("Option :: map_or", () => {
-  let x: Option<string>;
+    const list = [1, 2, 3, 4, 5];
 
-  x = Some("foo");
-  assert_eq!(
-    x.map_or(42, (v) => v.length),
-    3,
-  );
+    let has_inspected = false;
 
-  x = None;
-  assert_eq!(
-    x.map_or(42, (v) => v.length),
-    42,
-  );
-});
+    let x = get(list, 2).inspect((_v) => {
+      has_inspected = true;
+    });
 
-test("Option :: map_or_else", () => {
-  const k = 21;
-  let x: Option<string>;
+    assert_eq!(x, Some(3));
+    assert_eq!(has_inspected, true);
+  });
 
-  x = Some("foo");
-  assert_eq!(
-    x.map_or_else(
-      () => 2 * k,
-      (v) => v.length,
-    ),
-    3,
-  );
+  test("is_none", () => {
+    let x: Option<number>;
 
-  x = None;
-  assert_eq!(
-    x.map_or_else(
-      () => 2 * k,
-      (v) => v.length,
-    ),
-    42,
-  );
-});
+    x = Some(2);
+    assert_eq!(x.is_none(), false);
 
-test("Option :: or", () => {
-  let x: Option<number>;
-  let y: Option<number>;
+    x = None;
+    assert_eq!(x.is_none(), true);
+  });
 
-  x = Some(2);
-  y = None;
-  assert_eq!(x.or(y), Some(2));
+  test("is_none_or", () => {
+    let x: Option<number>;
 
-  x = None;
-  y = Some(100);
-  assert_eq!(x.or(y), Some(100));
+    x = Some(2);
+    assert_eq!(
+      x.is_none_or((v) => v > 1),
+      true,
+    );
 
-  x = Some(2);
-  y = Some(100);
-  assert_eq!(x.or(y), Some(2));
+    x = Some(0);
+    assert_eq!(
+      x.is_none_or((v) => v > 1),
+      false,
+    );
 
-  x = None;
-  y = None;
-  assert_eq!(x.or(y), None);
-});
+    x = None;
+    assert_eq!(
+      x.is_none_or((v) => v > 1),
+      true,
+    );
+  });
 
-test("Option :: or_else", () => {
-  let x: Option<string>;
-  let y: Option<string>;
+  test("is_some", () => {
+    let x: Option<number>;
 
-  x = Some("barbarians");
-  y = Some("vikings");
-  assert_eq!(
-    x.or_else(() => y),
-    Some("barbarians"),
-  );
+    x = Some(2);
+    assert_eq!(x.is_some(), true);
 
-  x = None;
-  y = Some("vikings");
-  assert_eq!(
-    x.or_else(() => y),
-    Some("vikings"),
-  );
+    x = None;
+    assert_eq!(x.is_some(), false);
+  });
 
-  x = None;
-  y = None;
-  assert_eq!(
-    x.or_else(() => y),
-    None,
-  );
-});
+  test("is_some_and", () => {
+    let x: Option<number>;
 
-test("Option :: toString", () => {
-  let x: Option<unknown>;
+    x = Some(2);
+    assert_eq!(
+      x.is_some_and((v) => v > 1),
+      true,
+    );
 
-  x = Some(true);
-  assert_eq!(x.toString(), "Some(true)");
+    x = Some(0);
+    assert_eq!(
+      x.is_some_and((v) => v > 1),
+      false,
+    );
 
-  x = Some(42);
-  assert_eq!(x.toString(), "Some(42)");
+    x = None;
+    assert_eq!(
+      x.is_some_and((v) => v > 1),
+      false,
+    );
+  });
 
-  x = Some("hello");
-  assert_eq!(x.toString(), "Some(hello)");
+  test("map", () => {
+    let x: Option<string>;
 
-  x = Some([1, 2]);
-  assert_eq!(x.toString(), "Some(1,2)");
+    x = Some("Hello, World!");
+    assert_eq!(
+      x.map((s) => s.length),
+      Some(13),
+    );
 
-  x = Some({});
-  assert_eq!(x.toString(), "Some([object Object])");
+    x = None;
+    assert_eq!(
+      x.map((s) => s.length),
+      None,
+    );
+  });
 
-  x = Some(() => 2 * 4);
-  assert_eq!(x.toString(), "Some(() => 2 * 4)");
+  test("map_or", () => {
+    let x: Option<string>;
 
-  x = None;
-  assert_eq!(x.toString(), "None");
-});
+    x = Some("foo");
+    assert_eq!(
+      x.map_or(42, (v) => v.length),
+      3,
+    );
 
-test("Option :: unwrap", () => {
-  let x: Option<string>;
+    x = None;
+    assert_eq!(
+      x.map_or(42, (v) => v.length),
+      42,
+    );
+  });
 
-  x = Some("air");
-  assert_eq!(x.unwrap(), "air");
+  test("map_or_else", () => {
+    const k = 21;
+    let x: Option<string>;
 
-  x = None;
-  assert_err!(
-    () => x.unwrap(),
-    TypeError,
-    "Called `Option.unwrap()` on a `None` value",
-  );
-});
+    x = Some("foo");
+    assert_eq!(
+      x.map_or_else(
+        () => 2 * k,
+        (v) => v.length,
+      ),
+      3,
+    );
 
-test("Option :: unwrap_or", () => {
-  let x: Option<number>;
+    x = None;
+    assert_eq!(
+      x.map_or_else(
+        () => 2 * k,
+        (v) => v.length,
+      ),
+      42,
+    );
+  });
 
-  x = Some(42);
-  assert_eq!(x.unwrap_or(1), 42);
+  test("or", () => {
+    let x: Option<number>;
+    let y: Option<number>;
 
-  x = None;
-  assert_eq!(x.unwrap_or(1), 1);
-});
+    x = Some(2);
+    y = None;
+    assert_eq!(x.or(y), Some(2));
 
-test("Option :: unwrap_or_else", () => {
-  const k = 10;
-  let x: Option<number>;
+    x = None;
+    y = Some(100);
+    assert_eq!(x.or(y), Some(100));
 
-  x = Some(4);
-  assert_eq!(
-    x.unwrap_or_else(() => 2 * k),
-    4,
-  );
+    x = Some(2);
+    y = Some(100);
+    assert_eq!(x.or(y), Some(2));
 
-  x = None;
-  assert_eq!(
-    x.unwrap_or_else(() => 2 * k),
-    20,
-  );
-});
+    x = None;
+    y = None;
+    assert_eq!(x.or(y), None);
+  });
 
-test("Option :: xor", () => {
-  let x: Option<number>;
-  let y: Option<number>;
+  test("or_else", () => {
+    let x: Option<string>;
+    let y: Option<string>;
 
-  x = Some(2);
-  y = None;
-  assert_eq!(x.xor(y), Some(2));
+    x = Some("barbarians");
+    y = Some("vikings");
+    assert_eq!(
+      x.or_else(() => y),
+      Some("barbarians"),
+    );
 
-  x = None;
-  y = Some(100);
-  assert_eq!(x.xor(y), Some(100));
+    x = None;
+    y = Some("vikings");
+    assert_eq!(
+      x.or_else(() => y),
+      Some("vikings"),
+    );
 
-  x = Some(2);
-  y = Some(100);
-  assert_eq!(x.xor(y), None);
+    x = None;
+    y = None;
+    assert_eq!(
+      x.or_else(() => y),
+      None,
+    );
+  });
 
-  x = None;
-  y = None;
-  assert_eq!(x.xor(y), None);
+  test("toString", () => {
+    let x: Option<unknown>;
+
+    x = Some(true);
+    assert_eq!(x.toString(), "Some(true)");
+
+    x = Some(42);
+    assert_eq!(x.toString(), "Some(42)");
+
+    x = Some("hello");
+    assert_eq!(x.toString(), "Some(hello)");
+
+    x = Some([1, 2]);
+    assert_eq!(x.toString(), "Some(1,2)");
+
+    x = Some({});
+    assert_eq!(x.toString(), "Some([object Object])");
+
+    x = Some(() => 2 * 4);
+    assert_eq!(x.toString(), "Some(() => 2 * 4)");
+
+    x = None;
+    assert_eq!(x.toString(), "None");
+  });
+
+  test("unwrap", () => {
+    let x: Option<string>;
+
+    x = Some("air");
+    assert_eq!(x.unwrap(), "air");
+
+    x = None;
+    assert_err!(
+      () => x.unwrap(),
+      TypeError,
+      "Called `Option.unwrap()` on a `None` value",
+    );
+  });
+
+  test("unwrap_or", () => {
+    let x: Option<number>;
+
+    x = Some(42);
+    assert_eq!(x.unwrap_or(1), 42);
+
+    x = None;
+    assert_eq!(x.unwrap_or(1), 1);
+  });
+
+  test("unwrap_or_else", () => {
+    const k = 10;
+    let x: Option<number>;
+
+    x = Some(4);
+    assert_eq!(
+      x.unwrap_or_else(() => 2 * k),
+      4,
+    );
+
+    x = None;
+    assert_eq!(
+      x.unwrap_or_else(() => 2 * k),
+      20,
+    );
+  });
+
+  test("xor", () => {
+    let x: Option<number>;
+    let y: Option<number>;
+
+    x = Some(2);
+    y = None;
+    assert_eq!(x.xor(y), Some(2));
+
+    x = None;
+    y = Some(100);
+    assert_eq!(x.xor(y), Some(100));
+
+    x = Some(2);
+    y = Some(100);
+    assert_eq!(x.xor(y), None);
+
+    x = None;
+    y = None;
+    assert_eq!(x.xor(y), None);
+  });
 });
--- tests/option.types.test.ts
+++ tests/option.types.test.ts
@@ -1,292 +1,294 @@
-import { test, expectTypeOf } from "vitest";
+import { describe, test, expectTypeOf } from "vitest";
 
 import { Some, None, Option } from "../src/option";
 
-test("Option :: and", () => {
-  let x: Option<number>;
-  let y: Option<string>;
+describe("Option", () => {
+  test("and", () => {
+    let x: Option<number>;
+    let y: Option<string>;
 
-  x = Some(2);
-  y = None;
-  expectTypeOf(x.and(y)).toEqualTypeOf<Option<string | number>>;
+    x = Some(2);
+    y = None;
+    expectTypeOf(x.and(y)).toEqualTypeOf<Option<string | number>>;
 
-  x = None;
-  y = Some("foo");
-  expectTypeOf(x.and(y)).toEqualTypeOf<Option<string | number>>;
+    x = None;
+    y = Some("foo");
+    expectTypeOf(x.and(y)).toEqualTypeOf<Option<string | number>>;
 
-  x = Some(2);
-  y = Some("foo");
-  expectTypeOf(x.and(y)).toEqualTypeOf<Option<string | number>>;
+    x = Some(2);
+    y = Some("foo");
+    expectTypeOf(x.and(y)).toEqualTypeOf<Option<string | number>>;
 
-  x = None;
-  y = None;
-  expectTypeOf(x.and(y)).toEqualTypeOf<Option<string | number>>;
-});
+    x = None;
+    y = None;
+    expectTypeOf(x.and(y)).toEqualTypeOf<Option<string | number>>;
+  });
 
-test("Option :: and_then", () => {
-  let x: Option<string>;
-  let y: Option<string>;
+  test("and_then", () => {
+    let x: Option<string>;
+    let y: Option<string>;
 
-  x = Some("some value");
-  y = None;
-  expectTypeOf(x.and_then(() => y)).toEqualTypeOf<Option<string>>;
+    x = Some("some value");
+    y = None;
+    expectTypeOf(x.and_then(() => y)).toEqualTypeOf<Option<string>>;
 
-  x = None;
-  y = Some("then value");
-  expectTypeOf(x.and_then(() => y)).toEqualTypeOf<Option<string>>;
+    x = None;
+    y = Some("then value");
+    expectTypeOf(x.and_then(() => y)).toEqualTypeOf<Option<string>>;
 
-  x = Some("some value");
-  y = Some("then value");
-  expectTypeOf(x.and_then(() => y)).toEqualTypeOf<Option<string>>;
+    x = Some("some value");
+    y = Some("then value");
+    expectTypeOf(x.and_then(() => y)).toEqualTypeOf<Option<string>>;
 
-  x = None;
-  y = None;
-  expectTypeOf(x.and_then(() => y)).toEqualTypeOf<Option<string>>;
-});
+    x = None;
+    y = None;
+    expectTypeOf(x.and_then(() => y)).toEqualTypeOf<Option<string>>;
+  });
 
-test("Option :: expect", () => {
-  let x: Option<number>;
+  test("expect", () => {
+    let x: Option<number>;
 
-  x = Some(42);
-  expectTypeOf(x.expect("should return number value")).toEqualTypeOf<number>;
-});
+    x = Some(42);
+    expectTypeOf(x.expect("should return number value")).toEqualTypeOf<number>;
+  });
 
-test("Option :: filter", () => {
-  function is_even(n: number): boolean {
-    return n % 2 == 0;
-  }
+  test("filter", () => {
+    function is_even(n: number): boolean {
+      return n % 2 == 0;
+    }
 
-  let x: Option<number>;
-  let y: Option<number>;
+    let x: Option<number>;
+    let y: Option<number>;
 
-  x = Some(3);
-  y = None;
-  expectTypeOf(x.filter(is_even)).toEqualTypeOf<Option<number>>;
-  expectTypeOf(y.filter(is_even)).toEqualTypeOf<Option<number>>;
-});
+    x = Some(3);
+    y = None;
+    expectTypeOf(x.filter(is_even)).toEqualTypeOf<Option<number>>;
+    expectTypeOf(y.filter(is_even)).toEqualTypeOf<Option<number>>;
+  });
 
-test("Option :: flatten", () => {
-  let x: Option<Option<number>>;
+  test("flatten", () => {
+    let x: Option<Option<number>>;
 
-  x = Some(Some(6));
-  expectTypeOf(x.flatten()).toEqualTypeOf<Option<number>>;
+    x = Some(Some(6));
+    expectTypeOf(x.flatten()).toEqualTypeOf<Option<number>>;
 
-  x = Some(None);
-  expectTypeOf(x.flatten()).toEqualTypeOf<Option<number>>;
+    x = Some(None);
+    expectTypeOf(x.flatten()).toEqualTypeOf<Option<number>>;
 
-  x = None;
-  expectTypeOf(x.flatten()).toEqualTypeOf<Option<number>>;
-});
+    x = None;
+    expectTypeOf(x.flatten()).toEqualTypeOf<Option<number>>;
+  });
 
-test("Option :: inspect", () => {
-  function get<T>(arr: T[], idx: number): Option<T> {
-    const item = arr.at(idx);
-    return item !== undefined ? Some(item) : None;
-  }
+  test("inspect", () => {
+    function get<T>(arr: T[], idx: number): Option<T> {
+      const item = arr.at(idx);
+      return item !== undefined ? Some(item) : None;
+    }
 
-  const list = [1, 2, 3, 4, 5];
+    const list = [1, 2, 3, 4, 5];
 
-  let x: Option<number>;
+    let x: Option<number>;
 
-  x = get(list, 1).inspect((_v) => console.log);
-  expectTypeOf(x).toEqualTypeOf<Option<number>>;
+    x = get(list, 1).inspect((_v) => console.log);
+    expectTypeOf(x).toEqualTypeOf<Option<number>>;
 
-  x = get(list, 9).inspect((_v) => console.log);
-  expectTypeOf(x).toEqualTypeOf<Option<number>>;
-});
+    x = get(list, 9).inspect((_v) => console.log);
+    expectTypeOf(x).toEqualTypeOf<Option<number>>;
+  });
 
-test("Option :: is_none", () => {
-  let x: Option<number>;
+  test("is_none", () => {
+    let x: Option<number>;
 
-  x = Some(2);
-  expectTypeOf(x.is_none()).toEqualTypeOf<boolean>;
+    x = Some(2);
+    expectTypeOf(x.is_none()).toEqualTypeOf<boolean>;
 
-  x = None;
-  expectTypeOf(x.is_none()).toEqualTypeOf<boolean>;
-});
+    x = None;
+    expectTypeOf(x.is_none()).toEqualTypeOf<boolean>;
+  });
 
-test("Option :: is_none_or", () => {
-  let x: Option<number>;
+  test("is_none_or", () => {
+    let x: Option<number>;
 
-  x = Some(2);
-  expectTypeOf(x.is_none_or((v) => v > 1)).toEqualTypeOf<boolean>;
+    x = Some(2);
+    expectTypeOf(x.is_none_or((v) => v > 1)).toEqualTypeOf<boolean>;
+
+    x = Some(0);
+    expectTypeOf(x.is_none_or((v) => v > 1)).toEqualTypeOf<boolean>;
 
-  x = Some(0);
-  expectTypeOf(x.is_none_or((v) => v > 1)).toEqualTypeOf<boolean>;
+    x = None;
+    expectTypeOf(x.is_none_or((v) => v > 1)).toEqualTypeOf<boolean>;
+  });
 
-  x = None;
-  expectTypeOf(x.is_none_or((v) => v > 1)).toEqualTypeOf<boolean>;
-});
+  test("is_some", () => {
+    let x: Option<number>;
 
-test("Option :: is_some", () => {
-  let x: Option<number>;
+    x = Some(2);
+    expectTypeOf(x.is_some()).toEqualTypeOf<boolean>;
 
-  x = Some(2);
-  expectTypeOf(x.is_some()).toEqualTypeOf<boolean>;
+    x = None;
+    expectTypeOf(x.is_some()).toEqualTypeOf<boolean>;
+  });
 
-  x = None;
-  expectTypeOf(x.is_some()).toEqualTypeOf<boolean>;
-});
+  test("is_some_and", () => {
+    let x: Option<number>;
 
-test("Option :: is_some_and", () => {
-  let x: Option<number>;
+    x = Some(2);
+    expectTypeOf(x.is_some_and((v) => v > 1)).toEqualTypeOf<boolean>;
 
-  x = Some(2);
-  expectTypeOf(x.is_some_and((v) => v > 1)).toEqualTypeOf<boolean>;
+    x = Some(0);
+    expectTypeOf(x.is_some_and((v) => v > 1)).toEqualTypeOf<boolean>;
 
-  x = Some(0);
-  expectTypeOf(x.is_some_and((v) => v > 1)).toEqualTypeOf<boolean>;
+    x = None;
+    expectTypeOf(x.is_some_and((v) => v > 1)).toEqualTypeOf<boolean>;
+  });
 
-  x = None;
-  expectTypeOf(x.is_some_and((v) => v > 1)).toEqualTypeOf<boolean>;
-});
+  test("map", () => {
+    let x: Option<string>;
 
-test("Option :: map", () => {
-  let x: Option<string>;
+    x = Some("Hello, World!");
+    expectTypeOf(x.map((s) => s.length)).toEqualTypeOf<Option<string | number>>;
 
-  x = Some("Hello, World!");
-  expectTypeOf(x.map((s) => s.length)).toEqualTypeOf<Option<string | number>>;
+    x = None;
+    expectTypeOf(x.map((s) => s.length)).toEqualTypeOf<Option<string | number>>;
+  });
 
-  x = None;
-  expectTypeOf(x.map((s) => s.length)).toEqualTypeOf<Option<string | number>>;
-});
+  test("map_or", () => {
+    let x: Option<string>;
 
-test("Option :: map_or", () => {
-  let x: Option<string>;
+    x = Some("foo");
+    expectTypeOf(x.map_or(42, (v) => v.length)).toEqualTypeOf<number>;
 
-  x = Some("foo");
-  expectTypeOf(x.map_or(42, (v) => v.length)).toEqualTypeOf<number>;
+    x = None;
+    expectTypeOf(x.map_or(42, (v) => v.length)).toEqualTypeOf<number>;
+  });
 
-  x = None;
-  expectTypeOf(x.map_or(42, (v) => v.length)).toEqualTypeOf<number>;
-});
+  test("map_or_else", () => {
+    const k = 21;
+    let x: Option<string>;
 
-test("Option :: map_or_else", () => {
-  const k = 21;
-  let x: Option<string>;
+    x = Some("foo");
+    expectTypeOf(
+      x.map_or_else(
+        () => 2 * k,
+        (v) => v.length,
+      ),
+    ).toEqualTypeOf<number>;
 
-  x = Some("foo");
-  expectTypeOf(
-    x.map_or_else(
-      () => 2 * k,
-      (v) => v.length,
-    ),
-  ).toEqualTypeOf<number>;
+    x = None;
+    expectTypeOf(
+      x.map_or_else(
+        () => 2 * k,
+        (v) => v.length,
+      ),
+    ).toEqualTypeOf<number>;
+  });
 
-  x = None;
-  expectTypeOf(
-    x.map_or_else(
-      () => 2 * k,
-      (v) => v.length,
-    ),
-  ).toEqualTypeOf<number>;
-});
+  test("or", () => {
+    let x: Option<number>;
+    let y: Option<number>;
 
-test("Option :: or", () => {
-  let x: Option<number>;
-  let y: Option<number>;
+    x = Some(2);
+    y = None;
+    expectTypeOf(x.or(y)).toEqualTypeOf<Option<number>>;
 
-  x = Some(2);
-  y = None;
-  expectTypeOf(x.or(y)).toEqualTypeOf<Option<number>>;
+    x = None;
+    y = Some(100);
+    expectTypeOf(x.or(y)).toEqualTypeOf<Option<number>>;
 
-  x = None;
-  y = Some(100);
-  expectTypeOf(x.or(y)).toEqualTypeOf<Option<number>>;
+    x = Some(2);
+    y = Some(100);
+    expectTypeOf(x.or(y)).toEqualTypeOf<Option<number>>;
 
-  x = Some(2);
-  y = Some(100);
-  expectTypeOf(x.or(y)).toEqualTypeOf<Option<number>>;
+    x = None;
+    y = None;
+    expectTypeOf(x.or(y)).toEqualTypeOf<Option<number>>;
+  });
 
-  x = None;
-  y = None;
-  expectTypeOf(x.or(y)).toEqualTypeOf<Option<number>>;
-});
+  test("or_else", () => {
+    let x: Option<string>;
+    let y: Option<string>;
 
-test("Option :: or_else", () => {
-  let x: Option<string>;
-  let y: Option<string>;
+    x = Some("barbarians");
+    y = Some("vikings");
+    expectTypeOf(x.or_else(() => y)).toEqualTypeOf<Option<string>>;
 
-  x = Some("barbarians");
-  y = Some("vikings");
-  expectTypeOf(x.or_else(() => y)).toEqualTypeOf<Option<string>>;
+    x = None;
+    y = Some("vikings");
+    expectTypeOf(x.or_else(() => y)).toEqualTypeOf<Option<string>>;
 
-  x = None;
-  y = Some("vikings");
-  expectTypeOf(x.or_else(() => y)).toEqualTypeOf<Option<string>>;
+    x = None;
+    y = None;
+    expectTypeOf(x.or_else(() => y)).toEqualTypeOf<Option<string>>;
+  });
 
-  x = None;
-  y = None;
-  expectTypeOf(x.or_else(() => y)).toEqualTypeOf<Option<string>>;
-});
+  test("toString", () => {
+    let x: Option<unknown>;
 
-test("Option :: toString", () => {
-  let x: Option<unknown>;
+    x = Some(true);
+    expectTypeOf(x.toString()).toEqualTypeOf<string>;
 
-  x = Some(true);
-  expectTypeOf(x.toString()).toEqualTypeOf<string>;
+    x = Some(42);
+    expectTypeOf(x.toString()).toEqualTypeOf<string>;
 
-  x = Some(42);
-  expectTypeOf(x.toString()).toEqualTypeOf<string>;
+    x = Some("hello");
+    expectTypeOf(x.toString()).toEqualTypeOf<string>;
 
-  x = Some("hello");
-  expectTypeOf(x.toString()).toEqualTypeOf<string>;
+    x = Some([1, 2]);
+    expectTypeOf(x.toString()).toEqualTypeOf<string>;
 
-  x = Some([1, 2]);
-  expectTypeOf(x.toString()).toEqualTypeOf<string>;
+    x = Some({});
+    expectTypeOf(x.toString()).toEqualTypeOf<string>;
 
-  x = Some({});
-  expectTypeOf(x.toString()).toEqualTypeOf<string>;
+    x = None;
+    expectTypeOf(x.toString()).toEqualTypeOf<string>;
+  });
 
-  x = None;
-  expectTypeOf(x.toString()).toEqualTypeOf<string>;
-});
+  test("unwrap", () => {
+    let x: Option<string>;
 
-test("Option :: unwrap", () => {
-  let x: Option<string>;
+    x = Some("air");
+    expectTypeOf(x.unwrap()).toEqualTypeOf<string>;
+  });
 
-  x = Some("air");
-  expectTypeOf(x.unwrap()).toEqualTypeOf<string>;
-});
+  test("unwrap_or", () => {
+    let x: Option<number>;
 
-test("Option :: unwrap_or", () => {
-  let x: Option<number>;
+    x = Some(42);
+    expectTypeOf(x.unwrap_or(1)).toEqualTypeOf<number>;
 
-  x = Some(42);
-  expectTypeOf(x.unwrap_or(1)).toEqualTypeOf<number>;
+    x = None;
+    expectTypeOf(x.unwrap_or(1)).toEqualTypeOf<number>;
+  });
 
-  x = None;
-  expectTypeOf(x.unwrap_or(1)).toEqualTypeOf<number>;
-});
+  test("unwrap_or_else", () => {
+    const k = 10;
+    let x: Option<number>;
 
-test("Option :: unwrap_or_else", () => {
-  const k = 10;
-  let x: Option<number>;
+    x = Some(4);
+    expectTypeOf(x.unwrap_or_else(() => 2 * k)).toEqualTypeOf<number>;
 
-  x = Some(4);
-  expectTypeOf(x.unwrap_or_else(() => 2 * k)).toEqualTypeOf<number>;
+    x = None;
+    expectTypeOf(x.unwrap_or_else(() => 2 * k)).toEqualTypeOf<number>;
+  });
 
-  x = None;
-  expectTypeOf(x.unwrap_or_else(() => 2 * k)).toEqualTypeOf<number>;
-});
+  test("xor", () => {
+    let x: Option<number>;
+    let y: Option<number>;
 
-test("Option :: xor", () => {
-  let x: Option<number>;
-  let y: Option<number>;
+    x = Some(2);
+    y = None;
+    expectTypeOf(x.xor(y)).toEqualTypeOf<Option<number>>;
 
-  x = Some(2);
-  y = None;
-  expectTypeOf(x.xor(y)).toEqualTypeOf<Option<number>>;
+    x = None;
+    y = Some(100);
+    expectTypeOf(x.xor(y)).toEqualTypeOf<Option<number>>;
 
-  x = None;
-  y = Some(100);
-  expectTypeOf(x.xor(y)).toEqualTypeOf<Option<number>>;
+    x = Some(2);
+    y = Some(100);
+    expectTypeOf(x.xor(y)).toEqualTypeOf<Option<number>>;
 
-  x = Some(2);
-  y = Some(100);
-  expectTypeOf(x.xor(y)).toEqualTypeOf<Option<number>>;
-
-  x = None;
-  y = None;
-  expectTypeOf(x.xor(y)).toEqualTypeOf<Option<number>>;
+    x = None;
+    y = None;
+    expectTypeOf(x.xor(y)).toEqualTypeOf<Option<number>>;
+  });
 });
