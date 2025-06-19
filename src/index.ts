51df9b2d8d5f9d18e688b424d71be58d5c3142f8
Roman Hnatiuk <hnatiukr@pm.me>
2025-06-19 09:22:50 +0200

    build!: set up package entry point

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @module unwrap-or
+ * @version 0.4.1-alpha
+ * @author Roman Hnatiuk <hnatiukr@pm.me>
+ * @see https://github.com/hnatiukr/unwrap-or
+ * @license MIT
+ */
+
+export { Some, None } from "./option";
+export type { Option } from "./option";
+
+export { Ok, Err } from "./result";
+export type { Result } from "./result";
