802781016fc46e82ea5b72c136a68cce9eda38ad
Roman Hnatiuk <hnatiukr@pm.me>
2025-11-11 12:35:49 +0100

    chore!: goodbye npm and github

---
+++ .github/readme.txt
@@ -0,0 +1,3 @@
+The unwrap-or library has [moved off Github](https://sfconservancy.org/GiveUpGitHub/).
+
+Please switch your git remotes over to https://codeberg.org/hnatiukr/unwrap-or
--- lib/mod.ts
+++
@@ -1,5 +0,0 @@
-export { None, Some } from "./option/option.ts";
-export type { Option } from "./option/option.ts";
-
-export { Err, Ok } from "./result/result.ts";
-export type { Result } from "./result/result.ts";
