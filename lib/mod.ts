68ccabb43651d695c421caffa0072e1763a75c58
Roman Hnatiuk <hnatiukr@pm.me>
2025-11-11 11:16:14 +0100

    chore: extend imported modules

---
+++ lib/mod.ts
@@ -0,0 +1,5 @@
+export { None, Some } from "./option/option.ts";
+export type { Option } from "./option/option.ts";
+
+export { Err, Ok } from "./result/result.ts";
+export type { Result } from "./result/result.ts";
